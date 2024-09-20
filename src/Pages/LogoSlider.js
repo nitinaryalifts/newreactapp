c1d865ed335c8b1e998577aaf16cc24af1ea7573
Nitin Arya <anon@Nitins-MacBook-Pro.local>
2024-09-20 16:59:21 +0530

    Your descriptive commit message

--- src/Pages/LogoSlider.js
+++ src/Pages/LogoSlider.js
@@ -8,6 +8,7 @@ import axios from 'axios';
 const LogoSlider = () => {
   const [clients, setClients] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [isEnd, setIsEnd] = useState(false); // State to track if the user reached the last slide
 
   useEffect(() => {
     const fetchClients = async () => {
@@ -35,12 +36,20 @@ const LogoSlider = () => {
       breakpoint: { max: 768, min: 0 },
       items: 3,
       slidesToSlide: 1,
-
     },
   };
 
+  // Function to detect if the last slide is reached
+  const handleAfterChange = (previousSlide, { currentSlide, totalItems }) => {
+    if (currentSlide === totalItems - 1) {
+      setIsEnd(true);  // Set to true when the last slide is reached
+    } else {
+      setIsEnd(false); // Reset when the user navigates back
+    }
+  };
+
   if (loading) {
-    return <p>.</p>;
+    return <p>Loading...</p>;
   }
 
   if (!clients.length) {
@@ -50,22 +59,31 @@ const LogoSlider = () => {
   return (
     <div className='bg-white m-0'>
       <section id='caros' className='section_padding past_e text-start py-5 w-100'>
-      <h3 className='heading'>Past Employers
-</h3>
-      <Carousel 
-        responsive={responsive} 
-        arrows={true}
-      >
-        {clients.map((client) => (
-          <div key={client._id} className='text-center'>
-            <img className='slideimg'
-              src={client.image.url} 
-              alt={client.name} 
-              style={{objectFit: 'cover' }} 
-            />
+        <h3 className='heading'>Past Employers</h3>
+        <Carousel 
+          responsive={responsive} 
+          arrows={true}
+          afterChange={handleAfterChange}  // Track when the user reaches the last slide
+        >
+          {clients.map((client) => (
+            <div key={client._id} className='text-center'>
+              <img className='slideimg'
+                src={client.image.url} 
+                alt={client.name} 
+                style={{objectFit: 'cover' }} 
+              />
+            </div>
+          ))}
+        </Carousel>
+
+        {/* Conditionally render a button when the last slide is reached */}
+        {isEnd && (
+          <div className="text-center mt-4">
+            <button className="btn btn-primary" onClick={() => alert('You have reached the end of the carousel!')}>
+              End of Carousel
+            </button>
           </div>
-        ))}
-      </Carousel>
+        )}
       </section>
     </div>
   );
